9877316bb01caa24db4c304238d058b4711c8f7f
SuryaX2 <sekharsurya111@gmail.com>
2024-02-09 17:24:42 +0530

    comitted

--- Projects/Weather/script.js
+++ Projects/Weather/script.js
@@ -1,20 +1,25 @@
-
 const apiKey = 'e10beb12b8592b7a7cffe2326d14c028';
 const searchBar = document.querySelector('#input');
 const searchBtn = document.querySelector('.s-btn');
 const weatherIcon = document.querySelector('.weather-icon');
+
 async function check(city) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
-    if (response.status === 404) {
+    
+    if (response.status == 404) {
         document.querySelector('.e-cont').style.display = "block";
+        document.querySelector('.e-cont').classList.add('ani');
+        document.querySelector('.container').classList.add('ani');
         document.querySelector('.weather').style.display = "none";
     }
     else {
         let data = await response.json();
+
         document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector('.city').innerHTML = data.name;
         document.querySelector('.humi').innerHTML = data.main.humidity + "%";
         document.querySelector('.wind').innerHTML = Math.round(data.wind.speed) + " Km/h";
+
         if (data.weather[0].main === "Drizzle")
             weatherIcon.src = "../../HTML_CSS/images/drizzle.png";
         else if (data.weather[0].main === "Mist")
@@ -27,10 +32,16 @@ async function check(city) {
             weatherIcon.src = "../../HTML_CSS/images/rain.png";
         else if (data.weather[0].main === "Snow")
             weatherIcon.src = "../../HTML_CSS/images/snow.png";
-        document.querySelector('.weather').style.display = "none";
-        document.querySelector('.e-cont').style.display = "block";
+
+        document.querySelector('.weather').style.display = "block";
+        document.querySelector('.e-cont').style.display = "none";
+
+        document.querySelector('.container').classList.add('ani');
+        document.querySelector('.weather').classList.add('ani');
     }
 }
+
 searchBtn.addEventListener('click', () => {
+    console.log(searchBar.value);
     check(searchBar.value);
 });

5b302b884f5413714ae7f39fc19471bdc49246e4
SuryaX2 <sekharsurya369@gmail.com>
2024-05-17 17:28:44 +0530

    index and signup

--- Full_Stack/backend/index.js
+++ Full_Stack/backend/index.js
@@ -7,7 +7,7 @@ import connectToMongo from "./db.js";
 import express from 'express';
 import cors from 'cors';
 import User from "./models/Node.js"
-const PORT = 4000;
+const PORT = 3001;
 
 connectToMongo()
 const app = express()
@@ -18,6 +18,38 @@ app.post('/register', (req, res) => {
     User.create(req.body)
         .then(employees => res.json(employees))
         .catch(err => res.json(err))
+})
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    User.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password === password) {
+                    res.json("Success")
+                }
+                else {
+                    res.json("wrong password")
+                }
+            }
+        }).catch(err => console.log(`Error Occured`)
+})
+
+app.post('/forgot', (req, res) => {
+    const { id } = req.params;
+    const { email, npassword } = req.body;
+    User.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password != npassword) {
+                    User.findByIdAndUpdate({ _id: id }, { password: npassword })
+                        .then(e => res.json("Success"))
+                }
+                else {
+                    res.json("fail")
+                }
+            }
+        }).catch(err => console.log(`Error Occured`)
 })
 
 app.listen(PORT, () => {

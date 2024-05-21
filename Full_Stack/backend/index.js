d52cc03daa33fcc8e69a72248e535aa554c50bb4
SuryaX2 <sekharsurya369@gmail.com>
2024-05-21 21:46:12 +0530

    index.js

--- Full_Stack/backend/index.js
+++ Full_Stack/backend/index.js
@@ -12,93 +12,128 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 const jwt_str = "We are hacker";
+const connectToMongo = require("./db")
+const express = require('express')
+const cors=require('cors')
+const User=require('./models/Note');
+const {body, validationResult}=require('express-validator');
+const b1=require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const jwt_str="We are hacker";
 
-app.post('/register', async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(500).json({ errors: errors.array() });
-    }
-    try {
-        let user = await User.findOne({ email: req.body.email });
-        if (user) {
-            return res.status(404).json({ error: "user already exits" })
-        }
-        const salt = await b1.genSalt(6);
-        const spass = await b1.hash(req.body.password, salt);
-        console.log(spass);
-        user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: spass,
-        });
-        const data = {
-            user: {
-                id: user.id
-            }
-        }
-        const authtoken = jwt.sign(data, jwt_str);
-        res.json(authtoken)
+connectToMongo()
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-        //res.json(user)
-    } catch (error) {
-        console.error(error.massage);
-        res.status(600).send("Some Error occured");
-    }
+app.post('/register',async (req,res)=>{
+  const errors=validationResult(req);
+  if(!errors.isEmpty()){
+     return res.status(500).json({errors:errors.array()});
+  }
+  try {
+     let user = await User.findOne({email: req.body.email});
+     if (user){
+        return res.status(404).json({error:"user already exits"})
+     }
+     const salt= await b1.genSalt(6);
+     const spass= await b1.hash(req.body.password, salt);
+     console.log(spass);
+     user =await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: spass,
+     });
+     const data={
+      user:{
+        id:user.id
+      }
+     }
+     const authtoken = jwt.sign(data, jwt_str);
+     res.json(authtoken)
 
+     //res.json(user)
+  } catch (error) {
+     console.error(error.massage);
+     res.status(600).send("Some Error occured");
+  }
+  
 })
 
-app.post('/login', [body('email', 'Enter a valid email').isEmail(),
-body('password', 'password cannot be empty').exists(),
-], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(500).json({ errors: errors.array() });
-    }
-    const { email, password } = req.body;
-    try {
-        let user = await User.findOne({ email });
-        if (!user) {
-            return res.status(405).json({ error: "user not exits" })
-        }
-        const passcom = await b1.compare(password, user.password);
-        console.log("DB Password = " + password)
-        console.log("Page Password = " + user.password)
-        if (!passcom) {
-            return res.status(405).json({ error: "password miss match" })
-        }
-        const data = {
-            user: {
-                id: user.id
-            }
-        }
-        const authtoken = jwt.sign(data, jwt_str);
-        res.json("success")
+app.post('/login',[body('email','Enter a valid email').isEmail(),
+body('password','password cannot be empty').exists(),
+], async (req,res)=>{
+  const errors=validationResult(req);
+  if(!errors.isEmpty()){
+     return res.status(500).json({errors:errors.array()});
+  }
+  const {email,password}=req.body;
+  try {
+     let user = await User.findOne({email});
+     if (!user){
+        return res.status(406).json({error:"user not exits"})
+     }
+     const passcom = await b1.compare(password, user.password);
+     console.log("Page Password = "+password)
+     console.log("DB Password = "+user.password)
+     if(!passcom){
+      return res.status(406).json({error:"password miss match"})
+     }
+     const data={
+      user:{
+        id:user.id
+      }
+     }
+     const authtoken = jwt.sign(data, jwt_str);
+     res.json("success")
 
-    } catch (error) {
-        console.error(error.massage);
-        res.status(700).send("Some Error occured");
-    }
-
+  } catch (error) {
+     console.error(error.massage);
+     res.status(700).send("Some Error occured");
+  }
+  
 })
 
-app.put('/forgot', (req, res) => {
-    const { id } = req.params;
-    const { email, npassword } = req.body;
-    User.findOne({ email: email })
-        .then(user => {
-            if (user) {
-                if (user.password != npassword) {
-                    User.Update({ email: email }, { password: npassword })
-                        .then(e => res.json("Success"))
-                }
-                else {
-                    res.json("fail")
-                }
-            }
-        }).catch(err => console.log(`Error Occured`))
+app.post('/forgot',(req,res) =>{
+  const {email, npassword} = req.body;
+  User.findOne({email: email})
+  .then(user =>{
+    if(user){
+      if(user.password != npassword){
+        User.Update({email: email},{password: npassword})
+        .then(e=> res.json("Success"))
+      }
+      else{
+        res.json("fail")
+      }
+    }
+  }).catch(err=> console.log(Error Occured))
 })
 
-app.listen(PORT, () => {
-    console.log("server is ready")
+app.get("/fetch-detail", async(req,res) => {
+  try{
+    const allUser = await User.find({});
+    res.send({status:"ok", data:allUser})
+  }catch(err){
+    console.log(err)
+  }
+})
+
+app.post("/deleteuser", async(req, res) => {
+  const {id} = req.body;
+  //console.log(userid);
+  try{
+    const demo= await User.deleteOne({_id: id});
+    //console.log(res.json());
+    res.status(200).send({x:"Delete"});
+    
+  }catch(err){
+    console.log(err);
+  }
+})
+
+
+app.listen(PORT,()=>{
+  console.log("server is ready")
 })

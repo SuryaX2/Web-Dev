9d3d6b4ce336770b7b2d3337d6a6d23cebf18ad5
SuryaX2 <sekharsurya369@gmail.com>
2024-05-25 19:35:45 +0530

    js

--- Full_Stack/frontend/myapp/src/home.js
+++ Full_Stack/frontend/myapp/src/home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import './Home.css';
 
 function Home() {
     const [data, setData] = useState([]);
@@ -10,37 +11,46 @@ function Home() {
         fetch("http://localhost:3001/fetch-detail", {
             method: "GET"
         })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data, "userData");
-                setData(data.data);
-            })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data, "userData");
+            setData(data.data);
+        })
     }, []);
 
     const deleteUser = (id, name) => {
         console.log("Deleting");
         if (window.confirm(`Are you sure you want to delete ${name}?`)) {
             axios.post('http://localhost:3001/deleteuser', { id })
-                .then((res) => {
-                    console.log(res.data);
-                    if (res.data.x === "Delete") {
-                        alert("Deleted Successfully");
-                        window.location.reload();
-                    }
-                });
+            .then((res) => {
+                console.log(res.data);
+                if (res.data.x === "Delete") {
+                    alert("Deleted Successfully");
+                    window.location.reload();
+                }
+            })
+            .catch((err) => {
+                console.error("Error deleting user:", err);
+                alert("Error deleting user");
+            });
         }
     };
 
     const updateUser = () => {
+        console.log("Updating user:", editUser);
         axios.post('http://localhost:3001/updateuser', editUser)
-            .then((res) => {
-                console.log(res.data);
-                if (res.data.x === "Update") {
-                    alert("Updated Successfully");
-                    setIsEditing(false);
-                    window.location.reload();
-                }
-            });
+        .then((res) => {
+            console.log(res.data);
+            if (res.data.x === "Update") {
+                alert("Updated Successfully");
+                setIsEditing(false);
+                window.location.reload();
+            }
+        })
+        .catch((err) => {
+            console.error("Error updating user:", err);
+            alert("Error updating user");
+        });
     };
 
     const handleEditClick = (user) => {

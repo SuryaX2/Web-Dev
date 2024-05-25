2925cf5216d8be6767dedf55a387442eb0a4a3b0
SuryaX2 <sekharsurya369@gmail.com>
2024-05-25 19:30:22 +0530

    js

--- Full_Stack/frontend/myapp/src/home.js
+++ Full_Stack/frontend/myapp/src/home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import './Home.css';
 
 function Home() {
     const [data, setData] = useState([]);
@@ -10,37 +11,37 @@ function Home() {
         fetch("http://localhost:3001/fetch-detail", {
             method: "GET"
         })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data, "userData");
-                setData(data.data);
-            })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data, "userData");
+            setData(data.data);
+        })
     }, []);
 
     const deleteUser = (id, name) => {
         console.log("Deleting");
         if (window.confirm(`Are you sure you want to delete ${name}?`)) {
             axios.post('http://localhost:3001/deleteuser', { id })
-                .then((res) => {
-                    console.log(res.data);
-                    if (res.data.x === "Delete") {
-                        alert("Deleted Successfully");
-                        window.location.reload();
-                    }
-                });
+            .then((res) => {
+                console.log(res.data);
+                if (res.data.x === "Delete") {
+                    alert("Deleted Successfully");
+                    window.location.reload();
+                }
+            });
         }
     };
 
     const updateUser = () => {
         axios.post('http://localhost:3001/updateuser', editUser)
-            .then((res) => {
-                console.log(res.data);
-                if (res.data.x === "Update") {
-                    alert("Updated Successfully");
-                    setIsEditing(false);
-                    window.location.reload();
-                }
-            });
+        .then((res) => {
+            console.log(res.data);
+            if (res.data.x === "Update") {
+                alert("Updated Successfully");
+                setIsEditing(false);
+                window.location.reload();
+            }
+        });
     };
 
     const handleEditClick = (user) => {
@@ -56,8 +57,8 @@ function Home() {
     return (
         <>
             <h2>Welcome Boss, here is your customers' data</h2>
-            <div className="card-body py-md-4">
-                <table style={{ width: 500 }}>
+            <div className="card-body">
+                <table>
                     <thead>
                         <tr>
                             <th>Id</th>
@@ -80,27 +81,23 @@ function Home() {
                         ))}
                     </tbody>
                 </table>
-            </div>
 
-            {isEditing && (
-                <div className="edit-form">
+                <div className={`edit-form ${isEditing ? 'active' : ''}`}>
                     <h3>Edit User</h3>
                     <form onSubmit={(e) => { e.preventDefault(); updateUser(); }}>
                         <label>
                             Name:
                             <input type="text" name="name" value={editUser.name} onChange={handleChange} required />
                         </label>
-                        <br />
                         <label>
                             Email:
                             <input type="email" name="email" value={editUser.email} onChange={handleChange} required />
                         </label>
-                        <br />
                         <button type="submit">Update</button>
                         <button type="button" onClick={() => setIsEditing(false)}>Cancel</button>
                     </form>
                 </div>
-            )}
+            </div>
         </>
     );
 }

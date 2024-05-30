6fbd67b8b49009bd68b825131b94afa07c5df21f
SuryaX2 <sekharsurya369@gmail.com>
2024-05-30 20:25:31 +0530

    index.js

--- Full_Stack/backend/index.js
+++ Full_Stack/backend/index.js
@@ -10,7 +10,7 @@ const PORT = 3001;
 connectToMongo();
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.post('/register', async (req, res) => {
     const errors = validationResult(req);

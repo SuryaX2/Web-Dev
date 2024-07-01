da7a436e74c6dab18b68b4eff0deb986f7ec75d4
SuryaX2 <sekharsurya369@gmail.com>
2024-07-02 00:02:48 +0530

    .

--- Full_Stack/backend/index.js
+++ Full_Stack/backend/index.js
@@ -1,5 +1,5 @@
 import connectToMongo from "./db.js"
-import express from 'express';
+import express from 'express'
 import cors from 'cors';
 import User from "./models/Node.js";
 import { body, validationResult } from "express-validator";
